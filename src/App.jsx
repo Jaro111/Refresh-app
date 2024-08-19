6fb83837809554e27e1be9744662758fa8a01258
Jaro111 <jarokark@protonmail.com>
2024-08-19 17:06:23 +0100

    enhancement: basic router created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Gallery } from "./pages/Gallery";
+import { AboutUs } from "./pages/AboutUs";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Footer } from "./components/Footer/Footer";
+
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <p>blabla</p>
-    </>
+    <BrowserRouter basename="">
+      <Navbar />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
