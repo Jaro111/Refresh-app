94bc1e479feb1ea9087d586532d228306818503e
Jaro111 <jarokark@protonmail.com>
2024-08-19 13:37:41 +0100

    another error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
