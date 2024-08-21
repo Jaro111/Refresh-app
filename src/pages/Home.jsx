bfc7e938baa962d7b01ef1069ea6f0d4e5b6d874
Jaro111 <jarokark@protonmail.com>
2024-08-21 12:22:52 +0100

    enhancement: Router done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
 import React from "react";
+import { PageCentre } from "../components/PageCentre/PageCentre";
 import "./Home.css";
 
 export const Home = () => {
-  return (
-    <>
-      <p>Home</p>
-    </>
-  );
+  return <PageCentre />;
 };
