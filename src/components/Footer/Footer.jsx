f099e4ab770f61af8796a8625ce7405eabae144c
Jaro111 <jarokark@protonmail.com>
2024-09-04 11:14:28 +0100

    style: active added to navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoCallSharp } from "react-icons/io5";
+
 import "./Footer.css";
 
 export const Footer = () => {
-  return <div className="FooterSpace"></div>;
+  return (
+    <div className="FooterSpace">
+      <div className="footerIcons">
+        <div className="facebookIconContainer">
+          <a
+            target="blank"
+            href="https://www.facebook.com/people/Glow-Paznokcie-Rosnowo/61556554966373/"
+          >
+            <FaFacebook className="facebookIcon" />
+          </a>
+        </div>
+        <div className="locationIconContainer">
+          <a target="_blank" href="https://maps.app.goo.gl/ZQzstpXX4gWEiBf3A">
+            <FaLocationDot className="locationIcon" />
+          </a>
+        </div>
+        <div className="callIconContainer">
+          <IoCallSharp className="callIcon" />
+          <p className="callContent">+48 0913210597</p>
+        </div>
+      </div>
+    </div>
+  );
 };
